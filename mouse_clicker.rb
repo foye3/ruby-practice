73b86ffab64d4978a51f33a9300295bc2f7628a0
Fan Zhang <zffoye@gmail.com>
2020-07-30 12:01:24 -0400

    update mouse clicker

--- mouse_clicker.rb
+++ mouse_clicker.rb
@@ -4,11 +4,14 @@ require 'rumouse'
 
 mouse = RuMouse.new
 
+puts 'Input number of minutes'
+limit = gets.chomp
+
 counter = 0
 loop do
   mouse.click 10, 10
   counter += 1
   puts "clicker done #{counter}"
   sleep(60)
-  break if counter > 60
+  break if counter > limit
 end
