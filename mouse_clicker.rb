f1072e45ac0d9e8af5716d8cc036f74fa5238862
Fan Zhang <zffoye@gmail.com>
2021-01-28 11:09:57 -0500

    mouse clicker pass param in command line

--- mouse_clicker.rb
+++ mouse_clicker.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
+# Example Usage:
+#   ruby mouse_clicker.rb 20
+#   will click once every minute for 20 minutes
+
 require 'rumouse'
 
 mouse = RuMouse.new
 
-puts 'Input number of minutes'
-limit = gets.chomp
-
+limit = ARGV[0]
 counter = 0
 loop do
   mouse.click 10, 10
   counter += 1
   puts "clicker done #{counter}"
   sleep(60)
-  break if counter > limit
+  break if counter >= limit
 end
